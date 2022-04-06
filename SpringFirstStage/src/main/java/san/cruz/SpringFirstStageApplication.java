a7ba050beabf125321f9a6c9afe4991563d77176
Brilliant10_08 <Brilliant10_08@Tomcat0810>
2022-04-06 17:14:40 +0530

    Second-Commit with ApplicationContext

--- SpringFirstStage/src/main/java/san/cruz/SpringFirstStageApplication.java
+++ SpringFirstStage/src/main/java/san/cruz/SpringFirstStageApplication.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
@@ -12,13 +14,15 @@ public class SpringFirstStageApplication {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(SpringFirstStageApplication.class, args);
-		Resource resource=new ClassPathResource("spring.xml");
-		BeanFactory beanFactory=new  XmlBeanFactory(resource);
-		Programm m1=beanFactory.getBean("NITIN",san.cruz.Programm.class);
-		
-		System.out.println(m1.getPgName());
-		System.out.println(m1.getPgID());
-		System.out.println(m1.getPgGameVersion());
+		//Resource resource=new ClassPathResource("spring.xml");
+		///BeanFactory beanFactory=new  XmlBeanFactory(resource);
+		//Programm m1=beanFactory.getBean("NITIN",san.cruz.Programm.class);
+		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+		Programm p1=(Programm) context.getBean("NITIN");
+	    System.out.println(p1);
+		// System.out.println(m1.getPgName());
+		//System.out.println(m1.getPgID());
+		//System.out.println(m1.getPgGameVersion());
 		
 		
 	}
